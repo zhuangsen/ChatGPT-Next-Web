61974610ea430c65c6dad0f74c28ee7f814f4e0d
madison <zhuangsen@live.com>
2023-04-30 10:49:40 +0800

    upadte

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
-export const OWNER = "Yidadaa";
+export const OWNER = "Madison";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const REPO_URL = `https://gpt.zhuangsen.live`;
+export const ISSUE_URL = `https://gpt.zhuangsen.live`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
